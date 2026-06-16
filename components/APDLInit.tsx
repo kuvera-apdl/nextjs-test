1a860a5908120e57534f7dc2ad26ee0cc2948beb
kirill <kirillsukhikh99@gmail.com>
2026-06-15 21:52:49 -0400

    update apdl

--- components/APDLInit.tsx
+++ components/APDLInit.tsx
@@ -1,10 +1,7 @@
 import { APDL } from '@apdl-oss/sdk';
 
-export const apdl = APDL.init({
-  endpoints: {
-    ingestion: process.env.NEXT_PUBLIC_APDL_INGESTION_ENDPOINT as string,
-    config: process.env.NEXT_PUBLIC_APDL_CONFIG_ENDPOINT as string,
-  },
+const apdl = APDL.init({
+  endpoint: process.env.NEXT_PUBLIC_APDL_URL as string,
   auth: {
     clientKey: process.env.NEXT_PUBLIC_APDL_CLIENT_KEY as string,
   },
